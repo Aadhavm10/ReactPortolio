1f6d6b4b55353e7941094c0f6fb955550411c2b7
Aadhavm10 <aadhavmanimurugan@gmail.com>
2025-09-11 16:59:02 -0500

    resume gon

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -234,15 +234,6 @@ const HeroContent = () => {
       className="flex flex-row items-center justify-center px-20 mt-40 w-full relative z-10"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-        <motion.div
-          variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-        >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-          <h1 className="Welcome-text text-[13px]">
-            Fullstack Developer Portfolio
-          </h1>
-        </motion.div>
 
         <motion.div
           variants={slideInFromLeft(0.5)}
@@ -292,12 +283,6 @@ const HeroContent = () => {
           </div>
         </motion.div>
 
-        <motion.a
-          variants={slideInFromLeft(1)}
-          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
-        >
-          Resume
-        </motion.a>
       </div>
 
       <motion.div
